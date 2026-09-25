openzim/gutenberg

Add author biographies and portraits from Wikipedia (reconciled onto main)
Squashes the author-details feature (originally spanning 11 commits plus a
merge of main) into the single reconciled result of that merge, so this PR
presents a clean, linear list on top of main for final review.

scraper/src/gutenberg2zim/constants.py
@@ -13,6 +13,12 @@ VERSION = __version__
 
 SCRAPER = f"{NAME} {VERSION}"
 
+# Descriptive User-Agent identifying this scraper to every HTTP server we
+# talk to. The default python-requests UA is rejected (HTTP 403) by some
+# hosts, e.g. Wikimedia's ws-export, whose policy requires identifying the
+# client. See https://meta.wikimedia.org/wiki/User-Agent_policy
+USER_AGENT = f"{NAME}/{VERSION} (+https://github.com/openzim/gutenberg)"
+
 logger = getLogger(NAME, level=logging.INFO)
 
 FAVICON_PATH = ROOT_DIR / "templates" / "favicon.png"

scraper/src/gutenberg2zim/sources/gutenberg/author_enricher.py
@@ -1,0 +1,149 @@
+"""Author details enrichment for Gutenberg works (bio + portrait).
+
+When the `--with-author-details` option is enabled, the Gutenberg pipeline
+enriches every author with a short biography and a portrait sourced from
+Wikipedia.
+
+The pivot from a Gutenberg author id to its Wikipedia article is the
+`pgterms:webpage` link that Project Gutenberg curates inside the author
+`pgterms:agent` block of the per-book RDF (see `sources.gutenberg.metadata`).
+A single call to the Wikipedia REST summary endpoint yields both the
+biography intro (`extract`) and the portrait thumbnail URL. This avoids any
+ambiguous-name matching and keeps the number of Wikipedia requests to at
+most one per author.
+
+We use the Wikipedia REST summary endpoint (page/summary) rather than the
+MediaWiki Action API because it is the endpoint designed for this exact need:
+it returns the article's plain-text intro and, when relevant, the portrait
+thumbnail in a single well-defined JSON response, without the parse/query
+boilerplate (templates, revisions, prop selection) the Action API requires.
+
+Author biographies are only fetched from the English Wikipedia for now: the
+REST summary URL is hard-coded to `en.wikipedia.org`, so authors whose
+`pgterms:webpage` points to another language (or to a non-Wikipedia page) are
+silently skipped until multi-language support is added.
+
+Authors without a Wikipedia link are left untouched, so a scrape never
+depends on Wikipedia being reachable.
+"""
+
+import json
+import re
+import urllib.parse
+from dataclasses import replace
+from threading import Lock
+
+import requests
+
+from gutenberg2zim.constants import logger
+from gutenberg2zim.core.concurrency import parallel_map
+from gutenberg2zim.core.download_engine import DownloadEngine
+from gutenberg2zim.core.models import Creator
+from gutenberg2zim.core.rewriters.image_rewriter import ImageProcessor
+from gutenberg2zim.core.work_store import WorkStore
+from gutenberg2zim.core.zim_assembler import ZimAssembler
+
+WIKIPEDIA_SUMMARY_URL = "https://en.wikipedia.org/api/rest_v1/page/summary/{title}"
+
+PORTRAIT_PATH_TEMPLATE = "authors/{id}.webp"
+
+
+def wikipedia_title(creator: Creator) -> str | None:
+    """Extract the Wikipedia article title from a creator's webpage link."""
+    webpage = creator.extra.get("webpage_resource")
+    if not webpage:
+        return None
+    match = re.match(r"https?://en\.wikipedia\.org/wiki/(.+)", webpage)
+    if not match:
+        return None
+    return urllib.parse.unquote(match.group(1))
+
+
+def fetch_author_summary(engine: DownloadEngine, title: str) -> dict | None:
+    """Return the Wikipedia REST summary JSON for `title`, or None on failure."""
+    url = WIKIPEDIA_SUMMARY_URL.format(title=urllib.parse.quote(title, safe="()"))
+    try:
+        response = engine.fetch_bytes(url)
+    except requests.RequestException as exc:
+        logger.warning(f"Failed to fetch Wikipedia summary for {title}: {exc}")
+        return None
+    try:
+        summary = json.loads(response)
+    except ValueError:
+        logger.warning(f"Invalid JSON in Wikipedia summary for {title}")
+        return None
+    if "extract" not in summary:
+        return None
+    return summary
+
+
+def enrich_creator(
+    creator: Creator,
+    engine: DownloadEngine,
+    assembler: ZimAssembler,
+) -> Creator:
+    """Return `creator` enriched with bio and portrait when available."""
+    title = wikipedia_title(creator)
+    if not title:
+        return creator
+    summary = fetch_author_summary(engine, title)
+    if summary is None:
+        return creator
+
+    extra = dict(creator.extra)
+    extract = summary.get("extract")
+    if isinstance(extract, str) and extract.strip():
+        extra["bio"] = extract
+
+    image = summary.get("thumbnail") or summary.get("originalimage")
+    source = image.get("source") if image else None
+    if isinstance(source, str) and source:
+        portrait_path = PORTRAIT_PATH_TEMPLATE.format(id=creator.id)
+        try:
+            data = engine.fetch_bytes(source)
+            # Wikipedia serves a raster thumbnail; store it as WebP like covers
+            data = ImageProcessor.optimize_image_content(data)
+            assembler.add_item_for(
+                path=portrait_path,
+                content=data,
+                mimetype="image/webp",
+                is_front=False,
+            )
+            extra["portrait_path"] = portrait_path
+        except (requests.RequestException, OSError, ValueError) as exc:
+            logger.warning(
+                f"Failed to fetch or store portrait for {creator.name}: {exc}"
+            )
+
+    return replace(creator, extra=extra) if extra != dict(creator.extra) else creator
+
+
+def enrich_authors(
+    store: WorkStore,
+    engine: DownloadEngine,
+    assembler: ZimAssembler,
+    *,
+    concurrency: int,
+) -> None:
+    """Fetch bio and portrait for every author that has a Wikipedia page."""
+    works = list(store.works)
+    unique: dict[str, Creator] = {}
+    for work in works:
+        for creator in work.creators:
+            unique.setdefault(creator.id, creator)
+
+    creators = list(unique.values())
+    logger.info(f"Enriching {len(creators)} author(s) from Wikipedia")
+
+    results: dict[str, Creator] = {}
+    results_lock = Lock()
+
+    def enrich_entry(creator: Creator) -> None:
+        enriched = enrich_creator(creator, engine, assembler)
+        with results_lock:
+            results[creator.id] = enriched
+
+    parallel_map(enrich_entry, creators, concurrency)
+
+    for work in works:
+        work.creators = [results.get(creator.id, creator) for creator in work.creators]

scraper/tests/test_author_enricher.py
@@ -1,0 +1,193 @@
+"""Tests for gutenberg author enrichment (bio + portrait from Wikipedia)."""
+
+import io
+from unittest.mock import MagicMock
+
+import requests
+from PIL import Image
+
+from gutenberg2zim.core.models import Creator, Work
+from gutenberg2zim.core.work_store import WorkStore
+from gutenberg2zim.sources.gutenberg.author_enricher import (
+    enrich_authors,
+    enrich_creator,
+    fetch_author_summary,
+    wikipedia_title,
+)
+
+SUMMARY_JSON = b'{"extract": "A short biography.", "thumbnail": {"source": "https://upload.wikimedia.org/thumb.jpg"}}'
+
+
+def _jpeg_bytes() -> bytes:
+    buffer = io.BytesIO()
+    Image.new("RGB", (2, 2), "red").save(buffer, format="JPEG")
+    return buffer.getvalue()
+
+
+def _arrow_creator() -> Creator:
+    return Creator(
+        id="68",
+        name="Jane Austen",
+        extra={
+            "first_names": "Jane",
+            "webpage_resource": "https://en.wikipedia.org/wiki/Jane_Austen",
+        },
+    )
+
+
+def _store_with_creator(creator: Creator) -> WorkStore:
+    store = WorkStore()
+    store.add(Work(id="1", source="gutenberg", title="Emma", creators=[creator]))
+    return store
+
+
+def test_wikipedia_title_extracts_english_wikipedia_pages():
+    assert wikipedia_title(_arrow_creator()) == "Jane_Austen"
+    assert wikipedia_title(Creator(id="1", name="No Link")) is None
+    assert (
+        wikipedia_title(
+            Creator(
+                id="2",
+                name="Personal Site",
+                extra={"webpage_resource": "https://example.org/about"},
+            )
+        )
+        is None
+    )
+
+
+def test_fetch_author_summary_returns_json():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.return_value = SUMMARY_JSON
+
+    summary = fetch_author_summary(engine, "Jane_Austen")
+    assert summary is not None
+    assert summary["extract"] == "A short biography."
+    engine.fetch_bytes.assert_called_once_with(
+        "https://en.wikipedia.org/api/rest_v1/page/summary/Jane_Austen"
+    )
+
+
+def test_fetch_author_summary_handles_missing_page_and_bad_payload():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.return_value = (
+        b'{"type": "https://mediawiki.org/wiki/HyperSwitch/errors/not_found"}'
+    )
+    assert fetch_author_summary(engine, "No_Such_Author") is None
+
+    engine.fetch_bytes.return_value = b"not json"
+    assert fetch_author_summary(engine, "Bad") is None
+
+    def raise_connection_error(*_args, **_kwargs):
+        raise requests.ConnectionError("offline")
+
+    engine.fetch_bytes.side_effect = raise_connection_error
+    assert fetch_author_summary(engine, "Offline") is None
+
+
+def test_enrich_creator_sets_bio_portrait_and_skips_without_link():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.side_effect = [SUMMARY_JSON, _jpeg_bytes()]
+    assembler = MagicMock(name="assembler")
+
+    enriched = enrich_creator(_arrow_creator(), engine, assembler)
+
+    assert enriched.extra["bio"] == "A short biography."
+    assert enriched.extra["portrait_path"] == "authors/68.webp"
+    assembler.add_item_for.assert_called_once()
+    call = assembler.add_item_for.call_args
+    assert call.kwargs["path"] == "authors/68.webp"
+    assert call.kwargs["mimetype"] == "image/webp"
+    assert isinstance(call.kwargs["content"], bytes) and call.kwargs["content"]
+
+    engine.fetch_bytes.reset_mock()
+    unchanged = enrich_creator(Creator(id="216", name="Anonymous"), engine, assembler)
+    assert unchanged == Creator(id="216", name="Anonymous")
+    engine.fetch_bytes.assert_not_called()
+
+
+def test_enrich_creator_without_portrait_keeps_bio_only():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.return_value = b'{"extract": "Only a bio here."}'
+    assembler = MagicMock(name="assembler")
+
+    enriched = enrich_creator(_arrow_creator(), engine, assembler)
+
+    assert enriched.extra["bio"] == "Only a bio here."
+    assert "portrait_path" not in enriched.extra
+    assembler.add_item_for.assert_not_called()
+
+
+def test_enrich_creator_recovers_from_portrait_errors():
+    engine = MagicMock(name="engine")
+
+    def side_effect(url, **_kwargs):
+        if url.startswith("https://en.wikipedia.org/api/rest_v1"):
+            return SUMMARY_JSON
+        raise requests.ConnectionError("image down")
+
+    engine.fetch_bytes.side_effect = side_effect
+    assembler = MagicMock(name="assembler")
+
+    enriched = enrich_creator(_arrow_creator(), engine, assembler)
+
+    assert enriched.extra["bio"] == "A short biography."
+    assert "portrait_path" not in enriched.extra
+    assembler.add_item_for.assert_not_called()
+
+
+def test_enrich_authors_updates_store_and_deduplicates():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.side_effect = [SUMMARY_JSON, _jpeg_bytes()]
+    assembler = MagicMock(name="assembler")
+
+    store = _store_with_creator(_arrow_creator())
+    store.add(
+        Work(
+            id="2",
+            source="gutenberg",
+            title="Pride and Prejudice",
+            creators=[_arrow_creator()],
+        )
+    )
+
+    enrich_authors(store, engine, assembler, concurrency=2)
+
+    creators = {work.creators[0].id: work.creators[0] for work in store.works}
+    assert creators["68"].extra["bio"] == "A short biography."
+    assert creators["68"].extra["portrait_path"] == "authors/68.webp"
+    assert engine.fetch_bytes.call_count == 2
+
+
+def test_enrich_authors_ignores_authors_without_wikipedia():
+    engine = MagicMock(name="engine")
+    assembler = MagicMock(name="assembler")
+    store = _store_with_creator(Creator(id="216", name="Anonymous"))
+
+    enrich_authors(store, engine, assembler, concurrency=1)
+
+    assert store.works[0].creators[0].extra == {}
+    engine.fetch_bytes.assert_not_called()
+    assembler.add_item_for.assert_not_called()
+
+
+def test_enrich_authors_keeps_running_when_some_authors_fail():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.side_effect = requests.ConnectionError("offline")
+    assembler = MagicMock(name="assembler")
+    store = _store_with_creator(_arrow_creator())
+
+    enrich_authors(store, engine, assembler, concurrency=1)
+
+    assert store.works[0].creators[0].extra.get("webpage_resource")
+    assert "bio" not in store.works[0].creators[0].extra
+
+
+def test_enrich_authors_handles_empty_store():
+    engine = MagicMock(name="engine")
+    assembler = MagicMock(name="assembler")
+
+    enrich_authors(WorkStore(), engine, assembler, concurrency=1)
+
+    engine.fetch_bytes.assert_not_called()
+    assembler.add_item_for.assert_not_called()

openzim/papers

scraper/src/papers2zim/constants.py
@@ -13,6 +13,12 @@ VERSION = __version__
 
 SCRAPER = f"{NAME} {VERSION}"
 
+# Descriptive User-Agent identifying this scraper to every HTTP server we
+# talk to. The default python-requests UA is rejected (HTTP 403) by some
+# hosts, e.g. Wikimedia's ws-export, whose policy requires identifying the
+# client. See https://meta.wikimedia.org/wiki/User-Agent_policy
+USER_AGENT = f"{NAME}/{VERSION} (+https://github.com/openzim/papers)"
+
 logger = getLogger(NAME, level=logging.INFO)
 
 FAVICON_PATH = ROOT_DIR / "templates" / "favicon.png"

scraper/src/papers2zim/sources/gutenberg/author_enricher.py
@@ -1,0 +1,149 @@
+"""Author details enrichment for Gutenberg works (bio + portrait).
+
+When the `--with-author-details` option is enabled, the Gutenberg pipeline
+enriches every author with a short biography and a portrait sourced from
+Wikipedia.
+
+The pivot from a Gutenberg author id to its Wikipedia article is the
+`pgterms:webpage` link that Project Gutenberg curates inside the author
+`pgterms:agent` block of the per-book RDF (see `sources.gutenberg.metadata`).
+A single call to the Wikipedia REST summary endpoint yields both the
+biography intro (`extract`) and the portrait thumbnail URL. This avoids any
+ambiguous-name matching and keeps the number of Wikipedia requests to at
+most one per author.
+
+We use the Wikipedia REST summary endpoint (page/summary) rather than the
+MediaWiki Action API because it is the endpoint designed for this exact need:
+it returns the article's plain-text intro and, when relevant, the portrait
+thumbnail in a single well-defined JSON response, without the parse/query
+boilerplate (templates, revisions, prop selection) the Action API requires.
+
+Author biographies are only fetched from the English Wikipedia for now: the
+REST summary URL is hard-coded to `en.wikipedia.org`, so authors whose
+`pgterms:webpage` points to another language (or to a non-Wikipedia page) are
+silently skipped until multi-language support is added.
+
+Authors without a Wikipedia link are left untouched, so a scrape never
+depends on Wikipedia being reachable.
+"""
+
+import json
+import re
+import urllib.parse
+from dataclasses import replace
+from threading import Lock
+
+import requests
+
+from papers2zim.constants import logger
+from papers2zim.core.concurrency import parallel_map
+from papers2zim.core.download_engine import DownloadEngine
+from papers2zim.core.models import Creator
+from papers2zim.core.rewriters.image_rewriter import ImageProcessor
+from papers2zim.core.work_store import WorkStore
+from papers2zim.core.zim_assembler import ZimAssembler
+
+WIKIPEDIA_SUMMARY_URL = "https://en.wikipedia.org/api/rest_v1/page/summary/{title}"
+
+PORTRAIT_PATH_TEMPLATE = "authors/{id}.webp"
+
+
+def wikipedia_title(creator: Creator) -> str | None:
+    """Extract the Wikipedia article title from a creator's webpage link."""
+    webpage = creator.extra.get("webpage_resource")
+    if not webpage:
+        return None
+    match = re.match(r"https?://en\.wikipedia\.org/wiki/(.+)", webpage)
+    if not match:
+        return None
+    return urllib.parse.unquote(match.group(1))
+
+
+def fetch_author_summary(engine: DownloadEngine, title: str) -> dict | None:
+    """Return the Wikipedia REST summary JSON for `title`, or None on failure."""
+    url = WIKIPEDIA_SUMMARY_URL.format(title=urllib.parse.quote(title, safe="()"))
+    try:
+        response = engine.fetch_bytes(url)
+    except requests.RequestException as exc:
+        logger.warning(f"Failed to fetch Wikipedia summary for {title}: {exc}")
+        return None
+    try:
+        summary = json.loads(response)
+    except ValueError:
+        logger.warning(f"Invalid JSON in Wikipedia summary for {title}")
+        return None
+    if "extract" not in summary:
+        return None
+    return summary
+
+
+def enrich_creator(
+    creator: Creator,
+    engine: DownloadEngine,
+    assembler: ZimAssembler,
+) -> Creator:
+    """Return `creator` enriched with bio and portrait when available."""
+    title = wikipedia_title(creator)
+    if not title:
+        return creator
+    summary = fetch_author_summary(engine, title)
+    if summary is None:
+        return creator
+
+    extra = dict(creator.extra)
+    extract = summary.get("extract")
+    if isinstance(extract, str) and extract.strip():
+        extra["bio"] = extract
+
+    image = summary.get("thumbnail") or summary.get("originalimage")
+    source = image.get("source") if image else None
+    if isinstance(source, str) and source:
+        portrait_path = PORTRAIT_PATH_TEMPLATE.format(id=creator.id)
+        try:
+            data = engine.fetch_bytes(source)
+            # Wikipedia serves a raster thumbnail; store it as WebP like covers
+            data = ImageProcessor.optimize_image_content(data)
+            assembler.add_item_for(
+                path=portrait_path,
+                content=data,
+                mimetype="image/webp",
+                is_front=False,
+            )
+            extra["portrait_path"] = portrait_path
+        except (requests.RequestException, OSError, ValueError) as exc:
+            logger.warning(
+                f"Failed to fetch or store portrait for {creator.name}: {exc}"
+            )
+
+    return replace(creator, extra=extra) if extra != dict(creator.extra) else creator
+
+
+def enrich_authors(
+    store: WorkStore,
+    engine: DownloadEngine,
+    assembler: ZimAssembler,
+    *,
+    concurrency: int,
+) -> None:
+    """Fetch bio and portrait for every author that has a Wikipedia page."""
+    works = list(store.works)
+    unique: dict[str, Creator] = {}
+    for work in works:
+        for creator in work.creators:
+            unique.setdefault(creator.id, creator)
+
+    creators = list(unique.values())
+    logger.info(f"Enriching {len(creators)} author(s) from Wikipedia")
+
+    results: dict[str, Creator] = {}
+    results_lock = Lock()
+
+    def enrich_entry(creator: Creator) -> None:
+        enriched = enrich_creator(creator, engine, assembler)
+        with results_lock:
+            results[creator.id] = enriched
+
+    parallel_map(enrich_entry, creators, concurrency)
+
+    for work in works:
+        work.creators = [results.get(creator.id, creator) for creator in work.creators]

scraper/tests/test_author_enricher.py
@@ -1,0 +1,193 @@
+"""Tests for gutenberg author enrichment (bio + portrait from Wikipedia)."""
+
+import io
+from unittest.mock import MagicMock
+
+import requests
+from PIL import Image
+
+from papers2zim.core.models import Creator, Work
+from papers2zim.core.work_store import WorkStore
+from papers2zim.sources.gutenberg.author_enricher import (
+    enrich_authors,
+    enrich_creator,
+    fetch_author_summary,
+    wikipedia_title,
+)
+
+SUMMARY_JSON = b'{"extract": "A short biography.", "thumbnail": {"source": "https://upload.wikimedia.org/thumb.jpg"}}'
+
+
+def _jpeg_bytes() -> bytes:
+    buffer = io.BytesIO()
+    Image.new("RGB", (2, 2), "red").save(buffer, format="JPEG")
+    return buffer.getvalue()
+
+
+def _arrow_creator() -> Creator:
+    return Creator(
+        id="68",
+        name="Jane Austen",
+        extra={
+            "first_names": "Jane",
+            "webpage_resource": "https://en.wikipedia.org/wiki/Jane_Austen",
+        },
+    )
+
+
+def _store_with_creator(creator: Creator) -> WorkStore:
+    store = WorkStore()
+    store.add(Work(id="1", source="gutenberg", title="Emma", creators=[creator]))
+    return store
+
+
+def test_wikipedia_title_extracts_english_wikipedia_pages():
+    assert wikipedia_title(_arrow_creator()) == "Jane_Austen"
+    assert wikipedia_title(Creator(id="1", name="No Link")) is None
+    assert (
+        wikipedia_title(
+            Creator(
+                id="2",
+                name="Personal Site",
+                extra={"webpage_resource": "https://example.org/about"},
+            )
+        )
+        is None
+    )
+
+
+def test_fetch_author_summary_returns_json():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.return_value = SUMMARY_JSON
+
+    summary = fetch_author_summary(engine, "Jane_Austen")
+    assert summary is not None
+    assert summary["extract"] == "A short biography."
+    engine.fetch_bytes.assert_called_once_with(
+        "https://en.wikipedia.org/api/rest_v1/page/summary/Jane_Austen"
+    )
+
+
+def test_fetch_author_summary_handles_missing_page_and_bad_payload():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.return_value = (
+        b'{"type": "https://mediawiki.org/wiki/HyperSwitch/errors/not_found"}'
+    )
+    assert fetch_author_summary(engine, "No_Such_Author") is None
+
+    engine.fetch_bytes.return_value = b"not json"
+    assert fetch_author_summary(engine, "Bad") is None
+
+    def raise_connection_error(*_args, **_kwargs):
+        raise requests.ConnectionError("offline")
+
+    engine.fetch_bytes.side_effect = raise_connection_error
+    assert fetch_author_summary(engine, "Offline") is None
+
+
+def test_enrich_creator_sets_bio_portrait_and_skips_without_link():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.side_effect = [SUMMARY_JSON, _jpeg_bytes()]
+    assembler = MagicMock(name="assembler")
+
+    enriched = enrich_creator(_arrow_creator(), engine, assembler)
+
+    assert enriched.extra["bio"] == "A short biography."
+    assert enriched.extra["portrait_path"] == "authors/68.webp"
+    assembler.add_item_for.assert_called_once()
+    call = assembler.add_item_for.call_args
+    assert call.kwargs["path"] == "authors/68.webp"
+    assert call.kwargs["mimetype"] == "image/webp"
+    assert isinstance(call.kwargs["content"], bytes) and call.kwargs["content"]
+
+    engine.fetch_bytes.reset_mock()
+    unchanged = enrich_creator(Creator(id="216", name="Anonymous"), engine, assembler)
+    assert unchanged == Creator(id="216", name="Anonymous")
+    engine.fetch_bytes.assert_not_called()
+
+
+def test_enrich_creator_without_portrait_keeps_bio_only():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.return_value = b'{"extract": "Only a bio here."}'
+    assembler = MagicMock(name="assembler")
+
+    enriched = enrich_creator(_arrow_creator(), engine, assembler)
+
+    assert enriched.extra["bio"] == "Only a bio here."
+    assert "portrait_path" not in enriched.extra
+    assembler.add_item_for.assert_not_called()
+
+
+def test_enrich_creator_recovers_from_portrait_errors():
+    engine = MagicMock(name="engine")
+
+    def side_effect(url, **_kwargs):
+        if url.startswith("https://en.wikipedia.org/api/rest_v1"):
+            return SUMMARY_JSON
+        raise requests.ConnectionError("image down")
+
+    engine.fetch_bytes.side_effect = side_effect
+    assembler = MagicMock(name="assembler")
+
+    enriched = enrich_creator(_arrow_creator(), engine, assembler)
+
+    assert enriched.extra["bio"] == "A short biography."
+    assert "portrait_path" not in enriched.extra
+    assembler.add_item_for.assert_not_called()
+
+
+def test_enrich_authors_updates_store_and_deduplicates():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.side_effect = [SUMMARY_JSON, _jpeg_bytes()]
+    assembler = MagicMock(name="assembler")
+
+    store = _store_with_creator(_arrow_creator())
+    store.add(
+        Work(
+            id="2",
+            source="gutenberg",
+            title="Pride and Prejudice",
+            creators=[_arrow_creator()],
+        )
+    )
+
+    enrich_authors(store, engine, assembler, concurrency=2)
+
+    creators = {work.creators[0].id: work.creators[0] for work in store.works}
+    assert creators["68"].extra["bio"] == "A short biography."
+    assert creators["68"].extra["portrait_path"] == "authors/68.webp"
+    assert engine.fetch_bytes.call_count == 2
+
+
+def test_enrich_authors_ignores_authors_without_wikipedia():
+    engine = MagicMock(name="engine")
+    assembler = MagicMock(name="assembler")
+    store = _store_with_creator(Creator(id="216", name="Anonymous"))
+
+    enrich_authors(store, engine, assembler, concurrency=1)
+
+    assert store.works[0].creators[0].extra == {}
+    engine.fetch_bytes.assert_not_called()
+    assembler.add_item_for.assert_not_called()
+
+
+def test_enrich_authors_keeps_running_when_some_authors_fail():
+    engine = MagicMock(name="engine")
+    engine.fetch_bytes.side_effect = requests.ConnectionError("offline")
+    assembler = MagicMock(name="assembler")
+    store = _store_with_creator(_arrow_creator())
+
+    enrich_authors(store, engine, assembler, concurrency=1)
+
+    assert store.works[0].creators[0].extra.get("webpage_resource")
+    assert "bio" not in store.works[0].creators[0].extra
+
+
+def test_enrich_authors_handles_empty_store():
+    engine = MagicMock(name="engine")
+    assembler = MagicMock(name="assembler")
+
+    enrich_authors(WorkStore(), engine, assembler, concurrency=1)
+
+    engine.fetch_bytes.assert_not_called()
+    assembler.add_item_for.assert_not_called()